d5b2e7b78a8cfe54560ef67ba011eae2b5d890b8
Bala Krishna Sanneboyain <129377896+BalaKrishnaS7@users.noreply.github.com>
2025-02-28 10:05:53 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -23,6 +23,8 @@ int main() {
         case '*':
             result = num1 * num2;
             break;
+         case'%':
+         result= num1 % num2;
         case '/':
             if (num2 == 0) {
                 printf("Error! Division by zero is not allowed.\n");
